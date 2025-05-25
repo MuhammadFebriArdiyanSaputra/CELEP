9e80acf1c3b6a27036921774b56ab40a9c4a8e3c
Anta Atrizki <112595371+atnatr@users.noreply.github.com>
2025-05-25 14:39:03 +0700

    Revert "Development"

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\LandingController;
-use App\Http\Controllers\AuthController;
 
-Route::get('/signin', [AuthController::class, 'showSignIn'])->name('signin');
-Route::get('/', [LandingController::class, 'index'])->name('home');
-Route::get('/signup', [AuthController::class, 'showSignUp'])->name('signup');
-Route::get('/signup-success', [AuthController::class, 'signupSuccess'])->name('signup.success');
-Route::post('/signup-submit', [AuthController::class, 'signupSubmit'])->name('signup.submit');
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
